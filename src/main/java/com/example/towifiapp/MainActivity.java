a8f7e79e0e3d821fa1b6a8e48959100a1f67ac08
zg <2878557425@qq.com>
2018-05-26 15:05:30 +0800

    增加了填写ip和端口的功能，但问题是一旦发送数据后就会关闭连接，导致无法收到雾浮起回传的消息

--- src/main/java/com/example/towifiapp/MainActivity.java
+++ src/main/java/com/example/towifiapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.towifiapp;
 
+import android.annotation.SuppressLint;
 import android.os.AsyncTask;
 import android.os.Handler;
 import android.os.Message;
@@ -7,6 +8,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -16,6 +18,7 @@ import java.lang.ref.WeakReference;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.net.SocketAddress;
+import java.util.Scanner;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -23,6 +26,8 @@ public class MainActivity extends AppCompatActivity {
     private TextView showstate;
     private TextView connectState;
     private TextView serveState;
+    private EditText ipAddress;
+    private EditText portAddress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,34 +35,53 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         init_view();
 
+        //这部分是重写的点击事件，原来是通过在布局界面绑定方法的，今天突然报错说找不到方法，也不知道为什么，不过还好重写一遍也不麻烦
+        sendMessage.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                new SendMessage().execute();
+            }
+        });
+
         //开启服务器
-        ReceiveMessage serve = new ReceiveMessage();
-        serve.setHandler(mhandler);
-        new Thread(serve).start();
+//        ReceiveMessage serve = new ReceiveMessage();
+//        serve.setHandler(mhandler);
+//        new Thread(serve).start();
 
 
     }
+
 
     void init_view() {
         sendMessage = findViewById(R.id.button);
         showstate = findViewById(R.id.textView);
         connectState = findViewById(R.id.textView2);
         serveState = findViewById(R.id.textView3);
+        ipAddress = findViewById(R.id.ipAddress);
+        portAddress = findViewById(R.id.portAddress);
     }
 
-    void sendmessage(View view) {
-        new SendMessage().execute();//开启多线程
-    }
+
+//这个函数原来是用来在属性里直接绑定按键方法的，由于不知道为什么报错，已经改为使用OnclickLinsier
+//    void sendmessage() {
+//        new SendMessage().execute();//开启多线程
+//    }
+
 
     //一个异步处理的内部类
-    class SendMessage extends AsyncTask<Void, Void, Boolean> {
+    //4.8日注释：我好像需要对这个类也进行弱引用处理，书上好像没说需要这样啊
+    //@SuppressLint("StaticFieldLeak")//这个标注是忽略指定的警告，用来暂时看一下,问题其实还在
+     class SendMessage extends AsyncTask<Void, Void, Boolean> {
+
 
         private Socket client;
-        private static final String url = "192.168.4.1";
-        private static final int port = 333;
-        private String str = "send from Android";
+        private String url ;
+        private int port;
+        private String str = "k";
         private PrintStream out = null;
         private boolean isConnect = false;
+        private boolean inputcorrect = true;
+        private boolean firstClick=true;
 
         //后台任务执行前进行操作
         @Override
@@ -73,17 +97,23 @@ public class MainActivity extends AppCompatActivity {
         protected void onPostExecute(Boolean aBoolean) {
             super.onPostExecute(aBoolean);
             //Toast.makeText(getApplicationContext(),"结尾开始执行",Toast.LENGTH_SHORT).show();
+            if(inputcorrect) {
 
-            if (isConnect) {
-                connectState.setText("服务器连接成功");
-            } else {
-                connectState.setText("服务器连接失败");
-            }
+                if (isConnect) {
+                    connectState.setText("连接成功");
+                } else {
+                    connectState.setText("连接失败");
+                }
 
-            if (aBoolean) {
-                showstate.setText("发送状态：发送成功");
-            } else {
-                showstate.setText("发送状态：发送失败");
+                if (aBoolean) {
+                    showstate.setText("发送成功");
+                } else {
+                    showstate.setText("发送失败");
+                }
+            }else{
+                connectState.setText("未连接");
+                showstate.setText("未发送");
+                Toast.makeText(getApplicationContext(),"请输入正确的ip和端口",Toast.LENGTH_SHORT).show();
             }
 
         }
@@ -92,25 +122,42 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected Boolean doInBackground(Void... voids) {
             try {
-                //建立套接字连接，设置超时
-                client = new Socket();
-                SocketAddress address = new InetSocketAddress(url, port);
-                client.connect(address, 1000);//获取连接状态
-                isConnect = client.isConnected();
 
-                //获取client连接的输出流，用于发送数据
-                out = new PrintStream(client.getOutputStream());
-                out.print(str);
-                out.flush();
-                client.close();
-                out.close();
+                if(!("".equals(ipAddress.getText().toString()))&&!("".equals(portAddress.getText().toString()))) {
+                    url = ipAddress.getText().toString();
+                    port = Integer.parseInt(portAddress.getText().toString());
 
+                    //建立套接字连接，设置超时
+                    client = new Socket();
+                    SocketAddress address = new InetSocketAddress(url, port);
+                    client.connect(address, 1000);//获取连接状态
+                    isConnect = client.isConnected();
+
+                    //这边在第一次点击后开启接收消息线程
+                    if (firstClick){
+                        ReceiveMessage receiveMessage = new ReceiveMessage(client);
+                        receiveMessage.setHandler(mhandler);
+                        new Thread(receiveMessage).start();
+                        firstClick = false;
+                    }
+                    //获取client连接的输出流，用于发送数据
+                    out = new PrintStream(client.getOutputStream());
+                    out.print(str);
+                    out.flush();
+                    client.close();
+                    out.close();
+                    return true;
+                }else{
+                    //Toast.makeText(getApplicationContext(), "请输入正确的ip和端口", Toast.LENGTH_SHORT).show();
+                    inputcorrect = false;
+                    return false;
+                }
 
             } catch (IOException e) {
                 return false;
             }
 
-            return true;
+
         }
     }
 
@@ -129,14 +176,14 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-    //用过弱引用防止内存泄露
+    //用弱引用防止内存泄露
     //定义一个Handler 的子类
     private static class Myhandler extends Handler {
         //持有对MainAcitvity类的弱引用
         private WeakReference<MainActivity> mactivity;
 
         //构造函数要求一个MainActivity类型的activity对象
-        public Myhandler(MainActivity acitvity) {
+        private Myhandler(MainActivity acitvity) {
             mactivity = new WeakReference<MainActivity>(acitvity);
         }
 
@@ -147,7 +194,7 @@ public class MainActivity extends AppCompatActivity {
             //重写Handler父类的方法，操作得到的activity中的view对象
             switch (msg.what) {
                 case 1:
-                    activity.serveState.setText("服务器传回" + msg.obj);
+                    activity.serveState.setText(""+msg.obj);
             }
         }
     }
--- src/main/java/com/example/towifiapp/ReceiveMessage.java
+++ src/main/java/com/example/towifiapp/ReceiveMessage.java
@@ -6,19 +6,26 @@ import java.io.DataInputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
+
 import android.os.Handler;
 
 /**
+ * 通过新建服务器接收消息，只适用于局域网
  * Created by 28785 on 2018/4/5.
+ * 改动为，从新建一个服务器接收消息变为获得原来的tcp连接从中读取消息，这样方便单片机方面编程以及连接到外网
+ * Created by 287785 on 2018/5/24
  */
 
 public class ReceiveMessage implements Runnable {
-    private ServerSocket serveSocket;
+//    private ServerSocket serveSocket;
     private DataInputStream in;
     private byte[] receive;
+//    private String response;
     private Handler handler;
-
-    public ReceiveMessage(){
+    private Socket client;
+    public ReceiveMessage(Socket client){
+        this.client = client;
     }
 
     public void setHandler(Handler handler){
@@ -28,27 +35,38 @@ public class ReceiveMessage implements Runnable {
     @Override
     public void run() {
         try {
-            serveSocket=new ServerSocket(5000);
+            //在5000端口设置监听服务
+//            serveSocket=new ServerSocket(5000);
+//            Socket client=null;
+            //监听端口消息
             while (true) {
-                Socket client = serveSocket.accept();
+//                client = serveSocket.accept();
                 in = new DataInputStream(client.getInputStream());
                 receive = new byte[20];
                 in.read(receive);
+                //使用Scanner类从流中读取消息
+//                Scanner in = new Scanner(client.getInputStream());
+//                response = in.nextLine();
                 in.close();
 
+                //发送监听到的消息给主线程
                 Message message = new Message();
                 message.what = 1;
                 message.obj = new String(receive);
+//                message.obj = response;
                 this.handler.sendMessage(message);
             }
 
         } catch (IOException e) {
             e.printStackTrace();
-        }try{
-            serveSocket.close();
-        }catch (IOException e){
-            e.printStackTrace();
         }
+//        try{
+//            if (serveSocket!=null)
+//            serveSocket.close();
+//        }catch (IOException e){
+//            e.printStackTrace();
+//        }
+
 
     }
 }
